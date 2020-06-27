437665de3d5f59a832a482c6fbb69f9f8721f322
cloudXA <zming794@gmail.com>
2020-06-27 12:00:45 +0800

    关联数据库管理 及其 相应的增删改查

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,30 +1,116 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Order = require('../models/order');
+const Product = require('../models/product');
 
 // Handle incoming  GET request to /orders 
 // 处理连入到/orders的GET请求
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Orders were fetched'
-  })
+  Order.find()
+    .select("product quantity _id")
+    .exec()
+    .then(docs => {
+      res.status(200).json({
+        count: docs.length,
+        orders: docs.map(doc => {
+          return {
+            _id: doc._id,
+            quantity: doc.quantity,
+            product: doc.product,
+            request: {
+              type: "GET",
+              url: 'http://localhost:3000/orders/' + doc._id
+            }
+          }
+        }),
+        
+      })
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      })
+    })
 })
 
+// 关联数据库
 router.post('/', (req, res, next) => {
-  const order = {
-    productId: req.body.productId,
-    quatity: req.body.quatity
-  }
-  res.status(201).json({
-    message: 'Order was created',
-    order: order
-  })
+  Product.findById(req.body.productId)
+    .then(product => {
+      if (!product) {
+        return res.status(404).json({
+          message: 'page not found'
+        })
+      }
+      const order = new Order({
+        _id: mongoose.Types.ObjectId(),
+        quantity: req.body.quantity,
+        product: req.body.productId
+      });
+      return order.save();
+    })
+    .then(result => {
+      res.status(201).json({
+        message: 'order created',
+        createdOrder: {
+          _id: result._id,
+          product: result.product,
+          quantity: result.quantity
+        },
+        request: {
+          type: 'GET',
+          url: 'http://localhost:3000/orders/' + result._id
+        }
+      })
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      })
+    })
 })
 
 router.get('/:orderId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Order was created',
-    orderId: req.params.orderId
-  })
+  Order.findById(req.params.orderId)
+    .exec()
+    .then(order => {
+      if(!order) {
+        res.status(404).json({
+          message: 'order not found'
+        })
+      }
+      res.status(200).json({
+        order: order,
+        request: {
+          type: 'GET',
+          url: 'http://localhost:3000/orders'
+        }
+      })
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      })
+    })
+})
+
+
+router.delete('/:orderId', (req, res, next) => {
+  Order.remove({ _id: req.params.orderId })
+    .exec()
+    .then(result => {
+      res.status(200).json({
+        message: 'Order deleted',
+        request: {
+          type: 'POST',
+          url: 'http://localhost:3000/orders',
+          body: { productId: 'ID', quantity: 'Number' }
+        }
+      })
+    })
 })
 
 module.exports = router;
